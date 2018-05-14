0451dfdd8a16c6d400aa44b8e240391e83532cd8
gleb <gleb.zhukel@gmail.com>
2018-05-14 08:39:56 +0300

    Mysql context. Screen saver. Prototype in singleton

---
+++ src/main/java/screensaver/solution4/Config.java
@@ -0,0 +1,35 @@
+package screensaver.solution4;
+
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "screensaver.solution4")
+public class Config {
+    @Bean
+    @Scope("periodical")
+    public Color color(){
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+    @Bean
+    public ColorFrame1 frame1(){
+        return new ColorFrame1() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true){
+            context.getBean(ColorFrame1.class).showOnRandomPlace();
+            Thread.sleep(100);
+
+        }
+    }
+}
